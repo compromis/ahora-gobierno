c80d051e89af36cdea8a68d5e33cc9d40cfe8261
Guillem <guillem@disedit.com>
2019-10-08 13:19:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <carousel :items="1" :autoplay="true" :nav="false" class="carousel">
+      <slide>
+        <p>¿Ahora, gobierno?</p>
+        <p class="delayed">¿Y en los últimos 4 meses? ¿Por qué no?</p>
+      </slide>
+      <slide>
+        <p>¿Quizás porque os han importado más las siglas que las personas?</p>
+      </slide>
+      <slide>
+        <p>¿Sabéis quien está pagando vuestra irresponsabilidad?</p>
+      </slide>
+      <slide>
+        <p>¿Los poderosos?</p>
+        <p>No, a esos no les hace falta un gobierno. De hecho, les molesta que alguien les controle.</p>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+import Slide from './components/Slide'
+
+export default {
+  name: 'app',
+  components: {
+    Slide,
+    carousel
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.carousel {
+  position: relative;
+}
+
+.owl-dots {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+
+.delayed {
+  animation: reveal;
+}
+</style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="slide">
+    <div class="slide-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'slide'
+  }
+</script>
+
+<style lang="scss" scoped>
+.slide {
+  height: 100vh;
+  background: #f31912;
+  color: #fff;
+  font-weight: bold;
+  font-size: 5rem;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &-content {
+    max-width: 1200px;
+  }
+}
+</style>
