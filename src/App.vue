3e81cacd76ae96ff2a9552769052f4aad47b1764
Carles <carles2612@gmail.com>
2019-10-25 14:00:52 +0200

    Translate text to valencian

--- src/App.vue
+++ src/App.vue
@@ -3,57 +3,57 @@
     <carousel v-if="lang === 'val'" :items="1" :autoplay="false" class="carousel">
       <slide>
         <p>¿Ahora, gobierno?</p>
-        <p class="delayed">¿Y en los últimos 4 meses?<br />¿Por qué no?</p>
+        <p class="delayed">I en els últims 4 mesos?<br />Per què no?</p>
         <slide-hint class="slide-hint" />
       </slide>
       <slide>
-        <p>¿Quizás porque os han importado más las siglas que las personas?</p>
+        <p>Potser perquè us han importat més les sigles que les persones?</p>
       </slide>
       <slide>
-        <p>¿Sabéis quien está pagando vuestra irresponsabilidad?</p>
+        <p>Sabeu qui està pagant la vostra irresponsabilitat?</p>
       </slide>
       <slide color="#000">
-        <p>¿Los poderosos?</p>
-        <p class="delayed smaller">No, a esos no les hace falta un gobierno. De hecho, les molesta que alguien les controle.</p>
+        <p>Els poderosos?</p>
+        <p class="delayed smaller">No, a eixos no els fa falta un govern. De fet, els molesta que algu els controle.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1461088945293-0c17689e48ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2702&q=80">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>Las personas</p>
-        <p class="smaller">Porque se bloquean fondos para educación, sanidad, dependencia o servicios sociales.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>Les persones</p>
+        <p class="smaller">Perquè es bloquen fons per a educació, sanitat, dependència o serveis socials.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1512405228606-d71b7aa8d110?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>La gente trabajadora</p>
-        <p class="smaller">Porque no se deroga la reforma laboral ni se activa la lucha contra la precariedad.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>La gent treballadora</p>
+        <p class="smaller">Perquè no es deroga la reforma laboral ni s'activa la luita contra la precarietat.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>Las mujeres</p>
-        <p class="smaller">Porque no se aprueban más medidas contra la violencia machista.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>Les dones</p>
+        <p class="smaller">Perquè no s'aproven més mesures contra la violència masclista.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1416169607655-0c2b3ce2e1cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>El medio ambiente</p>
-        <p class="smaller">Porque no se aprueba una Ley de lucha contra la Emergencia Climática.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGA:</h3>
+        <p>El medi ambient</p>
+        <p class="smaller">Perquè no s'aprova una Llei de lluita contra l'Emergència Climàtica.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1506377872008-6645d9d29ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>Jóvenes</p>
-        <p class="smaller">Porque no se se limita el precio del alquiler, ni se aumentan las becas.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>La gent jove</p>
+        <p class="smaller">Perquè no es limita el preu del lloguer, ni s'augmenten les beques.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1524067217094-584e94e61106?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>Mayores</p>
-        <p class="smaller">Porque no se garantizan unas pensiones dignas.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>La gent major</p>
+        <p class="smaller">Perquè no es garanteixen unes pensions dignes.</p>
       </slide>
       <slide color="#000" background="https://images.unsplash.com/photo-1561982479-c36896dad276?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80">
-        <h3>LA IRRESPONSABILIDAD DE NO QUERER FORMAR UN GOBIERNO PROGRESISTA LA PAGAN:</h3>
-        <p>Las personas LGTBI</p>
-        <p class="smaller">Porque sigue sin aprobarse una Ley Integral LGTBI y una Ley Trans, siguiendo el ejemplo de las leyes valencianas.</p>
+        <h3>LA IRRESPONSABILITAT DE NO VOLER FORMAR UN GOVERN PROGRESISTA LA PAGUEN:</h3>
+        <p>Las persones LGTBI</p>
+        <p class="smaller">Perquè segueix sense aprovar-se una Llei Integral LGTBI i una Llei Trans, seguint l'exemple de les lleis valencianes.</p>
       </slide>
       <slide color="#f7921e">
-        <p>Para salir del bloqueo.</p>
-        <p class="smaller">Para conseguir un gobierno que mejore la vida de las personas y no de los poderosos.</p>
+        <p>Per a eixir del bloqueig.</p>
+        <p class="smaller">Per a aconseguir un govern que millore la vida de les persones i no dels poderosos.</p>
         <p class="smaller">Acordar, la política útil</p>
         <a href="https://compromis.net" target="_blank" class="logo"><logo /></a>
       </slide>
